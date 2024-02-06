4c1a1695ba55e7318ec3828b085647bef3d9396b
LikoSetiawan <128354782+LikoSetiawan@users.noreply.github.com>
2024-02-07 02:40:38 +0800

    Initial commit

---
+++ BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Liko Setiawan on 07/02/24.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
